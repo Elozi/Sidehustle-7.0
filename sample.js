ed9f312ad92e74fdc25fec89d88d4e6962fbf4bb
Elozi <abbyjamezzz@gmail.com>
2023-04-02 21:59:26 -0700

    added samples of landing page

---
+++ sample.js
@@ -0,0 +1,16 @@
+  const menu = document.querySelector("#menu");
+  const navbar = document.querySelector(".navbar");
+  
+  menu.addEventListener("click", function() {
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("nav-toggle");
+    console.log("clicked")
+  });
+  
+ 
+
+  
+  
+  
+  
+  
